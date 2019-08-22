782c06902fedd15e152d151874f728c78f303274
Mohammed Foysal <mohammedfoysal@North.kainos.com>
2019-08-22 14:11:38 +0100

    Filter departments with pipe

--- employee-app/src/app/data.service.ts
+++ employee-app/src/app/data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Employee } from './employee';
 import { HttpClient } from '@angular/common/http';
+import { Department } from './department';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,7 @@ export class DataService {
   }
 
   employees = this.http.get<Employee[]>('/api/employees');
+  departments = this.http.get<Department[]>('/api/getalldepartments');
 
   // public addCity(newCity: City): void {
   //   this.cities = this.http.post<City[]>('/api/addcity', newCity);
