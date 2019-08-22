71663acbba9d0c739e94d2dcef4d6bfe3f69c3bc
Mohammed Foysal <mohammedfoysal@North.kainos.com>
2019-08-22 13:18:53 +0100

    Display real employees from hardcoded departments

--- employee-app/src/app/department-employees/department-employees.component.ts
+++ employee-app/src/app/department-employees/department-employees.component.ts
@@ -27,35 +27,9 @@ export class DepartmentEmployeesComponent implements OnInit {
   ngOnInit() {
     this.subDepartment = this.switchboard.department$.subscribe((d) => {
         this.department = d;
-        // this.employees = [
-        //   {
-        //     "id": 4,
-        //     "first_name": "first name",
-        //     "last_name": "last name",
-        //     "address_line": "address line here",
-        //     "post_code": "postcode here",
-        //     "email": "email here",
-        //     "nin": "nin here",
-        //     "bank_sort_code": "sort code here",
-        //     "bank_account_no": "account number here",
-        //     "salary": "salary here",
-        //   },
-        //   {
-        //     "id": 5,
-        //     "first_name": "bob name",
-        //     "last_name": "last name",
-        //     "address_line": "address line here",
-        //     "post_code": "postcode here",
-        //     "email": "email here",
-        //     "nin": "nin here",
-        //     "bank_sort_code": "sort code here",
-        //     "bank_account_no": "account number here",
-        //     "salary": "salary here",
-        //   }
-        // ];
-
-        this.employees = this.http.get<Employee[]>('/api/getemployeesbydepartment?id=' + this.department.department_id);
-        console.log(this.employees);
+        this.httpClient.get<Employee>('/api/getemployeesbydepartment?id=' + this.department.department_id).subscribe(data => {
+          this.employees = data;
+        });
     });
   }
 
